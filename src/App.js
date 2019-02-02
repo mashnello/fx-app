29df4f0bda6085584fddf6baecf49b34b1988ac3
Daniil Mashnenko <dmashnenko@Air-Daniil.home>
2019-02-02 02:00:32 +0100

    add icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import {  ReactComponent as CloseIcon } from './icons/close.svg';
+import {  ReactComponent as GraphIcon } from './icons/graph.svg';
+import {  ReactComponent as SwitchIcon } from './icons/switch.svg';
+import {  ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import {  ReactComponent as ArrowDownIcon } from './icons/arrow-down.svg';
 import './App.css';
 
 class App extends Component {
@@ -6,6 +11,13 @@ class App extends Component {
     return (
       <div className="app">
         <header>
+          <div className="icons">
+            <CloseIcon width={20} height={20} />
+            <GraphIcon width={20} height={20} />
+            <SwitchIcon width={20} height={20} />
+            <ArrowIcon width={20} height={20} />
+            <ArrowDownIcon width={20} height={20} />
+          </div>
           <h1 className="header">Exchange</h1>
         </header>
         <main>
@@ -47,6 +59,15 @@ class App extends Component {
             <button className="exchange-btn">Exchange</button>
           </div>
         </main>
+        <footer>
+          <div className="credits">
+            <div>Icons made by <a href="https://www.flaticon.com/authors/egor-rumyantsev" title="Egor Rumyantsev">Egor Rumyantsev</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+            <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+            <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+            <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+            <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+          </div>
+        </footer>
       </div>
     );
   }
