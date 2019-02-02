a8826b91f55c62c7344a3e3f893a36f122b091c8
Daniil Mashnenko <dmashnenko@Air-Daniil.home>
2019-02-02 01:14:31 +0100

    create basic layout

--- src/App.js
+++ src/App.js
@@ -5,9 +5,48 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>
-          Let's do it!
-        </h1>
+        <header>
+          <h1 className="header">Exchange</h1>
+        </header>
+        <main>
+          <div className="ccy ccy1">
+            <div className="ccy-wrapper">
+              <select className="ccy-list">
+                <option value="USD" selected>USD</option>
+                <option value="EUR">EUR</option>
+                <option value="GBP">GBP</option>
+              </select>
+              <input
+                className="ccy-value"
+                type="number"
+                placeholder="0"
+              />
+            </div>
+            <div className="balance">Balance: $0.00</div>
+          </div>
+          <div className="ccy-switch-wrapper">
+            <div className="ccy-switch"></div>
+            <div className="ccy-rate">$1 = zl4.2860</div>
+          </div>
+          <div className="ccy ccy2">
+            <div className="ccy-wrapper">
+              <select className="ccy-list">
+                <option value="USD" selected>USD</option>
+                <option value="EUR">EUR</option>
+                <option value="GBP">GBP</option>
+              </select>
+              <input
+                className="ccy-value"
+                type="number"
+                placeholder="0"
+              />
+            </div>
+            <div className="balance">Balance: $0.00</div>
+          </div>
+          <div className="exchange-btn-wrapper">
+            <button className="exchange-btn">Exchange</button>
+          </div>
+        </main>
       </div>
     );
   }
